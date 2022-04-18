635826c6f24012e8d28e37093ad5064d84c7a833
Matheus Bittencourt <matheus@ae.studio>
2022-04-18 12:00:03 -0300

    add prettier

--- App.tsx
+++ App.tsx
@@ -1,8 +1,8 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
-import ChatScreen from './src/screens/Chat';
-import ProfileScreen from './src/screens/Profile';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+import ChatScreen from "./src/screens/Chat";
+import ProfileScreen from "./src/screens/Profile";
 
 const Tab = createBottomTabNavigator();
 
@@ -10,16 +10,9 @@ export default function App() {
   return (
     <NavigationContainer>
       <Tab.Navigator>
-        <Tab.Screen
-          name="Chat"
-          component={ChatScreen}
-        />
-        <Tab.Screen
-          name="Profile"
-          component={ProfileScreen}
-        />
+        <Tab.Screen name="Chat" component={ChatScreen} />
+        <Tab.Screen name="Profile" component={ProfileScreen} />
       </Tab.Navigator>
     </NavigationContainer>
   );
 }
-
