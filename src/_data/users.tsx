4817154f0d9d64590a265b57ebc4cb61fa7193d1
Daniel Camargo <pererinha@gmail.com>
2022-04-20 18:38:52 -0300

    added fake data

---
+++ src/_data/users.tsx
@@ -0,0 +1,16 @@
+import { User } from "../types/User";
+
+export const Thiago: User = {
+  id: "thiago.snr",
+  name: "thiago",
+};
+
+export const Matheus: User = {
+  id: "matheus.snr",
+  name: "Matheus",
+};
+
+export const Daniel: User = {
+  id: "daniel.snr",
+  name: "Daniel",
+};
