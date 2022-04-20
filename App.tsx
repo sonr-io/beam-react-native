89bfebddb4bedaab0b54023f0f84e3e7d2abb66e
Matheus Bittencourt <matheus@ae.studio>
2022-04-20 10:03:05 -0300

    add navigation to chat screen

--- App.tsx
+++ App.tsx
@@ -1,4 +1,7 @@
+import "react-native-gesture-handler";
 import React from "react";
+import { StyleSheet } from "react-native";
+import { SafeAreaView } from "react-native-safe-area-context";
 import { NavigationContainer } from "@react-navigation/native";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import ChatScreen from "./src/screens/Chat";
@@ -8,11 +11,19 @@ const Tab = createBottomTabNavigator();
 
 export default function App() {
   return (
-    <NavigationContainer>
-      <Tab.Navigator>
-        <Tab.Screen name="Chat" component={ChatScreen} />
-        <Tab.Screen name="Profile" component={ProfileScreen} />
-      </Tab.Navigator>
-    </NavigationContainer>
+    <SafeAreaView style={styles.container}>
+      <NavigationContainer>
+        <Tab.Navigator screenOptions={{ headerShown: false }}>
+          <Tab.Screen name="Chat" component={ChatScreen} />
+          <Tab.Screen name="Profile" component={ProfileScreen} />
+        </Tab.Navigator>
+      </NavigationContainer>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+  },
+});
--- src/screens/Chat.tsx
+++ src/screens/Chat.tsx
@@ -1,10 +1,56 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import { TouchableOpacity } from "react-native-gesture-handler";
+import {
+  createStackNavigator,
+  StackScreenProps,
+} from "@react-navigation/stack";
+
+const Stack = createStackNavigator();
+
+const ids = ["1", "2", "3", "4"];
+
+type Params = {
+  List: {};
+  View: { id: string };
+};
+
+type ChatListProps = StackScreenProps<Params, "List">;
+
+const ChatList = ({ navigation }: ChatListProps) => (
+  <View style={styles.listContainer}>
+    <Text style={styles.listTitle}>Messages</Text>
+    <View style={styles.list}>
+      {ids.map((id) => (
+        <TouchableOpacity
+          key={id}
+          style={styles.button}
+          onPress={() => navigation.navigate("View", { id })}
+        >
+          <Text>{id}</Text>
+        </TouchableOpacity>
+      ))}
+    </View>
+  </View>
+);
+
+type ChatViewProps = StackScreenProps<Params, "View">;
+
+const ChatView = ({ route }: ChatViewProps) => (
+  <View style={styles.container}>
+    <Text>{route.params.id}</Text>
+  </View>
+);
 
 const ChatScreen = () => (
-  <View style={styles.container}>
-    <Text>Chat screen</Text>
-  </View>
+  <Stack.Navigator>
+    <Stack.Screen
+      name="List"
+      component={ChatList}
+      options={{ headerShown: false, title: "Messages" }}
+    />
+    <Stack.Screen name="View" component={ChatView} options={{ title: "" }} />
+  </Stack.Navigator>
 );
 
 const styles = StyleSheet.create({
@@ -13,6 +59,28 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+  },
+  listContainer: {
+    flex: 1,
+    backgroundColor: "#fff",
+    paddingHorizontal: 20,
+    paddingTop: 25,
+  },
+  listTitle: {
+    alignSelf: "center",
+    fontSize: 22,
+    fontWeight: "bold",
+    marginBottom: 20,
+  },
+  list: {
+    alignItems: "stretch",
+  },
+  button: {
+    padding: 12,
+    borderWidth: 1,
+    borderColor: "#d2d3dd",
+    marginBottom: 16,
+    borderRadius: 10,
   },
 });
 
