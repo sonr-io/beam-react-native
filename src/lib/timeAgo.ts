969f35c26a40dfc90b58a05af74cbb9497c54aae
Daniel Camargo <pererinha@gmail.com>
2022-04-20 18:39:20 -0300

    added method to handle date (timeAgo)

---
+++ src/lib/timeAgo.ts
@@ -0,0 +1,20 @@
+import { format } from 'date-fns'
+
+export const timeAgo = (date: number) => {
+  const millisecondsPerMinute = 60 * 1000;
+  const millisecondsPerHour = millisecondsPerMinute * 60;
+  const millisecondsPerDay = millisecondsPerHour * 24;
+  const millisecondsPerWeek = millisecondsPerDay * 7;
+
+  const elapsed = new Date().getTime() - date;
+
+  if (elapsed < millisecondsPerDay) {
+    return format(new Date(date), 'hh:mm aa')
+  } else if (elapsed < millisecondsPerDay * 2) {
+    return 'yesterday'
+  } else if (elapsed < millisecondsPerWeek) {
+    return format(new Date(date), 'EEEE')
+  } else {
+    return format(new Date(date), 'MM/dd/yyyy')
+  }
+};
