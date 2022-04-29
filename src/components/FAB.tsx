41c02149458c4fe290c21c2b33b46edf4984ae00
Thiago Fassina <thiagoaaa@gmail.com>
2022-04-29 18:32:07 -0300

    Position FAB

--- src/components/FAB.tsx
+++ src/components/FAB.tsx
@@ -1,12 +1,37 @@
-import React from "react";
-import { Button } from "react-native";
+import React from 'react';
+import { StyleSheet, View } from 'react-native';
+import { TouchableOpacity } from 'react-native-gesture-handler';
+
+import NewChat from '../icons/NewChat';
 
 interface Props {
   onPress: () => void;
 }
 
 const FAB: React.FC<Props> = ({ onPress }) => {
-  return <Button title="New Chat" onPress={onPress} />;
+  return (
+    <View style={styles.container}>
+      <TouchableOpacity style={styles.button} onPress={onPress}>
+        <NewChat />
+      </TouchableOpacity>
+    </View>
+  );
 };
+
+const styles = StyleSheet.create({
+  container: {
+    position: 'absolute',
+    bottom: 10,
+    right: 25,
+  },
+  button: {
+    backgroundColor: 'blue',
+    width: 56,
+    height: 56,
+    borderRadius: 30,
+    alignItems:'center',
+    justifyContent: 'center',
+  },
+});
 
 export { FAB };
