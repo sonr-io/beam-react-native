a0dc1a42ffc67e746b12f4ab3d5bded3f30e318f
Thiago Fassina <thiagoaaa@gmail.com>
2022-05-02 14:53:16 -0300

    Add X button on new chat screen

--- src/screens/Chat/ChatNew.tsx
+++ src/screens/Chat/ChatNew.tsx
@@ -1,6 +1,13 @@
 import { StackScreenProps } from "@react-navigation/stack";
 import React from "react";
-import { Button, StyleSheet, Text, TextInput, View } from "react-native";
+import {
+  Button,
+  StyleSheet,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  View,
+} from "react-native";
 
 import { Params } from ".";
 import { GradientHeader } from "../../components/GradientHeader";
@@ -10,7 +17,17 @@ type Props = StackScreenProps<Params, "ChatNew">;
 const ChatNew: React.FC<Props> = ({ navigation }) => {
   return (
     <View style={styles.listContainer}>
-      <GradientHeader text="New Chat" />
+      <View style={styles.headerWithButton}>
+        <View style={styles.title}>
+          <GradientHeader text="New Chat" />
+        </View>
+        <TouchableOpacity
+          style={styles.headerButton}
+          onPress={() => navigation.goBack()}
+        >
+          <Text style={{ fontSize: 24 }}>×</Text>
+        </TouchableOpacity>
+      </View>
       <TextInput style={styles.nameInput} />
       <Button
         title="Start chat"
@@ -25,6 +42,17 @@ const styles = StyleSheet.create({
     flex: 1,
     backgroundColor: "#FFFFFF",
     paddingHorizontal: 20,
+  },
+  headerWithButton: {
+    display: "flex",
+    flexDirection: "row",
+  },
+  title: {
+    flex: 1,
+  },
+  headerButton: {
+    padding: 20,
+    marginBottom: 10,
   },
   nameInput: {
     borderColor: "#B1B5C3",
