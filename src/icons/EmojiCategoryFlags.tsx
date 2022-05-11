e050bbede865df89c0d379205e1d85375681f8d3
Daniel Camargo <pererinha@gmail.com>
2022-05-11 18:54:48 -0300

    added icons for each emoji category

---
+++ src/icons/EmojiCategoryFlags.tsx
@@ -0,0 +1,16 @@
+import React from "react";
+import Svg, { Path } from "react-native-svg";
+
+const EmojiCategoryFlags = () => (
+  <Svg width={16} height={16} viewBox="0 0 16 16" fill="none">
+    <Path
+      fillRule="evenodd"
+      clipRule="evenodd"
+      d="M3 15H2V1.294h1c.284.33.643.599 1.048.767.406.169.846.241 1.286.212a5.148 5.148 0 002.258-.676A6.035 6.035 0 0110 1c.819.008 1.621.22 2.333.616l.667.43v7.838l-.667-.431A4.893 4.893 0 0010 8.833a5.702 5.702 0 00-2.069.615 6.364 6.364 0 01-2.6.616A3.363 3.363 0 013 9.374V15z"
+      fill="#88849C"
+      fillOpacity={0.5}
+    />
+  </Svg>
+);
+
+export default EmojiCategoryFlags;
---
+++ src/icons/EmojiCategoryHistory.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Svg, { ClipPath, Defs, G, Path } from "react-native-svg";
+
+const EmojiCategoryHistory = () => (
+  <Svg width={12} height={12} viewBox="0 0 12 12" fill="none">
+    <G clipPath="url(#clip0_2687_10719)" fill="#88849C">
+      <Path d="M6 12a6 6 0 116-6 6.007 6.007 0 01-6 6zM6 .75a5.25 5.25 0 100 10.5A5.25 5.25 0 006 .75z" />
+      <Path d="M6.5 6.492H3v-.75h2.75V1.5h.75v4.992z" />
+    </G>
+    <Defs>
+      <ClipPath id="clip0_2687_10719">
+        <Path fill="#88849C" d="M0 0H12V12H0z" />
+      </ClipPath>
+    </Defs>
+  </Svg>
+);
+
+export default EmojiCategoryHistory;
