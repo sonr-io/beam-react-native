84b8cf66fe28552ae205ae50d81e19e671bc111e
Thiago Fassina <thiagoaaa@gmail.com>
2022-05-12 16:29:11 -0300

    Menu on message menu (#42)
    * Partial: add static menu buttons
    
    * Move emoji selector inside menu
    
    * Partial: extract emoji preset
    
    * External control to render emoji selector
    
    * Improve emoji preset and "expand" button

--- src/components/EmojiSelector.tsx
+++ src/components/EmojiSelector.tsx
@@ -33,15 +33,14 @@ const EmojiItem = ({ emoji, onSelectEmoji }: EmojiItemProps) => (
   </TouchableOpacity>
 );
 
-type EmojiReactionsProps = { onSelectEmoji: (emoji: string) => void };
+type EmojiSelectorProps = { onSelectEmoji: (emoji: string) => void };
 
-export const EmojiReactions = ({ onSelectEmoji }: EmojiReactionsProps) => {
+export const EmojiSelector = ({ onSelectEmoji }: EmojiSelectorProps) => {
   const emojiCategorySmile: EmojiCategory = {
     symbol: "😀",
     name: "Smileys & Emotion",
   };
 
-  const [showEmojis, setShowEmojis] = useState(false);
   const [emojis, setEmojis] = useState<Emoji[]>([]);
   const [selectedCategory, setSelectedCategory] =
     useState<EmojiCategory>(emojiCategorySmile);
@@ -105,85 +104,54 @@ export const EmojiReactions = ({ onSelectEmoji }: EmojiReactionsProps) => {
 
   return (
     <View style={styles.wrapper}>
-      <View style={styles.container}>
-        {defaultReactions.map((name) => {
-          return emoji
-            .filter((e) => e.name === name)
-            .map((e) => {
-              return (
-                <EmojiItem
-                  key={e.unified}
-                  emoji={e as Emoji}
-                  onSelectEmoji={handleSelectEmoji}
-                />
-              );
-            });
-        })}
-
-        <TouchableOpacity
-          activeOpacity={0.5}
-          style={styles.emojiItem}
-          onPress={() => {
-            setShowEmojis(!showEmojis);
-          }}
-        >
-          <Plus />
-        </TouchableOpacity>
-      </View>
-      {showEmojis && (
-        <>
-          <View style={[styles.container, styles.categories]}>
-            {categories.map((category) => {
-              return (
-                <TouchableOpacity
-                  onPress={() => {
-                    setSelectedCategory(category);
-                  }}
-                  key={category.name}
-                  activeOpacity={0.5}
-                  style={[
-                    styles.emojiItem,
-                    ,
-                    category.name === selectedCategory.name
-                      ? styles.selectedCategory
-                      : null,
-                  ]}
-                >
-                  <Text style={styles.emojiCategoryItemText}>
-                    {category.symbol}
-                  </Text>
-                </TouchableOpacity>
-              );
-            })}
-          </View>
-          <View style={styles.emojiList}>
-            <FlatList
-              data={emojis}
-              renderItem={({ item }) => (
-                <EmojiItem
-                  key={item.unified}
-                  emoji={item}
-                  onSelectEmoji={handleSelectEmoji}
-                />
-              )}
-              keyExtractor={(_, index) => index.toString()}
-              numColumns={8}
-            />
-          </View>
-        </>
-      )}
+      <>
+        <View style={[styles.container, styles.categories]}>
+          {categories.map((category) => {
+            return (
+              <TouchableOpacity
+                onPress={() => {
+                  setSelectedCategory(category);
+                }}
+                key={category.name}
+                activeOpacity={0.5}
+                style={[
+                  styles.emojiItem,
+                  ,
+                  category.name === selectedCategory.name
+                    ? styles.selectedCategory
+                    : null,
+                ]}
+              >
+                <Text style={styles.emojiCategoryItemText}>
+                  {category.symbol}
+                </Text>
+              </TouchableOpacity>
+            );
+          })}
+        </View>
+        <View style={styles.emojiList}>
+          <FlatList
+            data={emojis}
+            renderItem={({ item }) => (
+              <EmojiItem
+                key={item.unified}
+                emoji={item}
+                onSelectEmoji={handleSelectEmoji}
+              />
+            )}
+            keyExtractor={(_, index) => index.toString()}
+            numColumns={8}
+          />
+        </View>
+      </>
     </View>
   );
 };
 
 const styles = StyleSheet.create({
   wrapper: {
-    marginLeft: 40,
-    marginRight: 40,
-    marginTop: 40,
     alignSelf: "center",
     padding: 5,
-    backgroundColor: "#ffffff99",
     borderTopRightRadius: 12,
     borderTopLeftRadius: 4,
   },
--- src/screens/Chat/MessageMenu.tsx
+++ src/screens/Chat/MessageMenu.tsx
@@ -1,25 +1,44 @@
 import { StackScreenProps } from "@react-navigation/stack";
-import React from "react";
-import { Platform, StyleSheet, View } from "react-native";
+import React, { useState } from "react";
+import {
+  Platform,
+  StyleSheet,
+  TouchableOpacity,
+  View,
+  Text,
+} from "react-native";
 import { TouchableWithoutFeedback } from "react-native-gesture-handler";
 import { useSafeAreaInsets } from "react-native-safe-area-context";
 
 import { Params } from ".";
 import BlurView from "../../components/BlurView";
-import { EmojiReactions } from "../../components/EmojiReactions";
+import { EmojiSelector } from "../../components/EmojiSelector";
+import IconPlus from "../../icons/Plus";
 import { MessageBubble } from "../../components/MessageBubble";
 import { useChatContext } from "../../contexts/ChatContext";
 import { useUserContext } from "../../contexts/UserContext";
 
 const ios = Platform.OS === "ios";
 
-type Props = StackScreenProps<Params, "MessageMenu">;
+type ReactionProps = {
+  emoji: string;
+  onPress: (emoji: string) => void;
+};
+const EmojiReaction = ({ emoji, onPress }: ReactionProps) => {
+  return (
+    <TouchableOpacity style={styles.emojiButton} onPress={() => onPress(emoji)}>
+      <Text style={styles.emojiDisplay}>{emoji}</Text>
+    </TouchableOpacity>
+  );
+};
 
+type Props = StackScreenProps<Params, "MessageMenu">;
 const MessageMenu: React.FC<Props> = ({ navigation, route }) => {
   const { message, chatId } = route.params;
   const insets = useSafeAreaInsets();
   const { user } = useUserContext();
   const { addReaction } = useChatContext();
+  const [showEmojiSelector, setShowEmojiSelector] = useState(false);
   const pushEmoji = (emoji: string) => {
     addReaction(chatId, message.id, emoji);
   };
@@ -44,7 +63,45 @@ const MessageMenu: React.FC<Props> = ({ navigation, route }) => {
           reactions={message.reactions.map((r) => r.emoji)}
         />
       </View>
-      <EmojiReactions onSelectEmoji={(emoji) => pushEmoji(emoji)} />
+
+      <View style={styles.menuContainer}>
+        <BlurView>
+          <View style={styles.emojiPreset}>
+            <EmojiReaction emoji="👍" onPress={(e) => pushEmoji(e)} />
+            <EmojiReaction emoji="✅" onPress={(e) => pushEmoji(e)} />
+            <EmojiReaction emoji="❤" onPress={(e) => pushEmoji(e)} />
+            <EmojiReaction emoji="☕" onPress={(e) => pushEmoji(e)} />
+            <EmojiReaction emoji="📅" onPress={(e) => pushEmoji(e)} />
+            <EmojiReaction emoji="💥" onPress={(e) => pushEmoji(e)} />
+            <EmojiReaction emoji="😎" onPress={(e) => pushEmoji(e)} />
+
+            <TouchableOpacity
+              onPress={() => setShowEmojiSelector(!showEmojiSelector)}
+              style={styles.emojiShowSelector}
+            >
+              <IconPlus />
+            </TouchableOpacity>
+          </View>
+
+          {showEmojiSelector && (
+            <EmojiSelector onSelectEmoji={(e) => pushEmoji(e)} />
+          )}
+
+          {!showEmojiSelector && (
+            <>
+              <TouchableOpacity style={styles.menuButton}>
+                <Text style={styles.menuButtonText}>Reply</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.menuButton}>
+                <Text style={styles.menuButtonText}>Forward</Text>
+              </TouchableOpacity>
+              <TouchableOpacity style={styles.menuButton}>
+                <Text style={styles.menuButtonText}>Copy</Text>
+              </TouchableOpacity>
+            </>
+          )}
+        </BlurView>
+      </View>
     </BlurView>
   );
 };
@@ -72,6 +129,45 @@ const styles = StyleSheet.create({
   touchableBackground: {
     width: "100%",
     height: "100%",
+  },
+
+  menuContainer: {
+    width: 300,
+    marginTop: 40,
+    overflow: "hidden",
+    borderTopLeftRadius: 4,
+    borderTopRightRadius: 12,
+    borderBottomRightRadius: 4,
+    borderBottomLeftRadius: 12,
+  },
+  emojiPreset: {
+    flexDirection: "row",
+    width: "100%",
+    paddingVertical: 8,
+    paddingHorizontal: 15,
+  },
+  emojiShowSelector: {
+    flex: 1,
+    alignItems: "flex-end",
+    paddingTop: 4,
+    paddingRight: 4,
+  },
+  emojiButton: {
+    padding: 4,
+  },
+  emojiDisplay: {
+    fontSize: 20,
+  },
+  menuButton: {
+    borderTopWidth: 1,
+    borderTopColor: "#88849C20",
+    marginHorizontal: 15,
+    paddingVertical: 15,
+  },
+  menuButtonText: {
+    color: "#88849C",
+    fontFamily: "THICCCBOI_Bold",
+    fontSize: 16,
   },
 });
 
