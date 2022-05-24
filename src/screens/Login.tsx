5c03715649d5753eec6941414fd3f086883e80cf
Thiago Fassina <thiagoaaa@gmail.com>
2022-05-24 10:56:52 -0300

    Add some css to temp login

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,7 +1,7 @@
 import { USERS } from "@env";
 import { StackScreenProps } from "@react-navigation/stack";
 import React from "react";
-import { Button, StyleSheet, View } from "react-native";
+import { Button, StyleSheet, Text, TouchableOpacity, View } from "react-native";
 import { TextInput } from "react-native-gesture-handler";
 
 import { StackParams } from "../../App";
@@ -20,10 +20,16 @@ const LoginScreen: React.FC<Props> = ({ navigation }) => {
   const [user, setUser] = React.useState("");
   const [password, setPassword] = React.useState("");
   const presetUsers: PresetUser[] = JSON.parse(USERS || "[]");
+  const [error, setError] = React.useState(false);
 
-  const onLogin = async (_user?: string, _password?: string) => {
-    const client = await login(_user ?? user, _password ?? password);
-    setClient(client);
+const onLogin = async (_user?: string, _password?: string) => {
+  try {
+      const client = await login(_user ?? user, _password ?? password);
+      setClient(client);
+      setError(false);
+    } catch (_) {
+      setError(true);
+    }
   };
 
   return (
@@ -38,6 +44,10 @@ const LoginScreen: React.FC<Props> = ({ navigation }) => {
         />
       ))}
       <TextInput
+        autoFocus
+        autoCapitalize={"none"}
+        autoCorrect={false}
+        style={styles.input}
         placeholder="username"
         onChangeText={(text) => {
           setUser(text);
@@ -45,23 +55,30 @@ const LoginScreen: React.FC<Props> = ({ navigation }) => {
       />
       <TextInput
         secureTextEntry
+        style={styles.input}
         placeholder="password"
         onChangeText={(text) => {
           setPassword(text);
         }}
       />
-      <Button
-        title="login"
-        onPress={() => {
-          onLogin();
-        }}
-      />
-      <Button
-        title="skip"
-        onPress={() => {
-          navigation.push("Chat", {});
-        }}
-      />
+      {error && (
+        <Text style={{ marginBottom: 20, color: "darkred" }}>
+          Incorrect credentials
+        </Text>
+      )}
+      <View style={{ flexDirection: "row", marginHorizontal: -10 }}>
+        <TouchableOpacity style={styles.button} onPress={() => onLogin()}>
+          <Text style={styles.buttonText}>Login</Text>
+        </TouchableOpacity>
+        <TouchableOpacity
+          style={styles.button}
+          onPress={() => {
+            navigation.push("Chat", {});
+          }}
+        >
+          <Text style={styles.buttonText}>Skip</Text>
+        </TouchableOpacity>
+      </View>
     </View>
   );
 };
@@ -72,6 +89,24 @@ const styles = StyleSheet.create({
     backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
+    paddingHorizontal: 20,
+  },
+  input: {
+    borderWidth: 1,
+    borderRadius: 4,
+    padding: 10,
+    marginBottom: 20,
+    width: "100%",
+  },
+  button: {
+    flex: 1,
+    backgroundColor: "dodgerblue",
+    marginHorizontal: 10,
+    padding: 10,
+  },
+  buttonText: {
+    textAlign: "center",
+    color: "white",
   },
 });
 
