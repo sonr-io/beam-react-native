7d93603af0443176253dcf2f18c34101f35a0912
Thiago Fassina <thiagoaaa@gmail.com>
2022-05-26 16:58:58 -0300

    Use matchAll lib instead of string method (not available on Android)

--- @types/allTypes.d.ts
+++ @types/allTypes.d.ts
@@ -1,1 +1,2 @@
 declare module "react-native-opengraph-kit";
+declare module "string.prototype.matchall";
--- src/lib/splitMessageText.ts
+++ src/lib/splitMessageText.ts
@@ -1,8 +1,9 @@
 import { normalizeUrl } from "./normalizeUrl";
+import matchAll from "string.prototype.matchall";
 
 export const splitMessageText = (text: string) => {
   const regexp = /(([^  \n,\(\))]+)([ \n,\(\)]*))+?/g;
-  const matches = [...text.matchAll(regexp)];
+  const matches = [...matchAll(text, regexp)];
   return matches.map((group) => ({
     word: group[2],
     separator: group[3],
