5574e8e40e26e574f9bf3d3f897791b1fc26a224
Matheus Bittencourt <matheus@ae.studio>
2022-06-28 12:03:18 -0300

    show randomized colors on the avatars

--- src/components/Avatar/Avatar.tsx
+++ src/components/Avatar/Avatar.tsx
@@ -1,16 +1,33 @@
 import React from "react";
 import { StyleSheet, Text, View } from "react-native";
+import seedrandom from "seedrandom";
 
 import { User } from "../../types/Chat";
+
+const backgroundColors = [
+  "#7744E3",
+  "#0053B5",
+  "#009C82",
+  "#1D1A27",
+  "#C20037",
+];
+
+const getColor = (id: string) => {
+  const rng = seedrandom(id);
+  const max = backgroundColors.length;
+  const index = Math.floor(rng() * max);
+  return backgroundColors[index];
+};
 
 interface Props {
   user: User;
 }
 
 export const Avatar: React.FC<Props> = ({ user }) => {
+  const color = getColor(user.id);
   const initials = user.id.charAt(1).toUpperCase() + user.id.charAt(2);
   return (
-    <View style={styles.avatar}>
+    <View style={[styles.avatar, { backgroundColor: color }]}>
       <Text style={styles.initials}>{initials}</Text>
     </View>
   );
@@ -24,7 +41,6 @@ const styles = StyleSheet.create({
     height: 45,
     borderRadius: 40,
     marginRight: 12,
-    backgroundColor: "#1C1C3B",
   },
   initials: {
     color: "#F6F5FA",
